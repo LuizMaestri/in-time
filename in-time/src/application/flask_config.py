e70e9122b20557831af4f04cec8efea675d4985e
Luiz Maestri <luizricardomaestri@gmail.com>
2017-01-05 22:46:48 -0200

    fix config file's name

--- in-time/src/application/flask_config.py
+++ in-time/src/application/flask_config.py
@@ -2,5 +2,5 @@ from flask_api import FlaskAPI
 import os
 
 app = FlaskAPI(__name__)
-config_file = os.path.join(app.instance_path, 'application.cfg')
+config_file = os.path.join(app.instance_path, 'config.cfg')
 app.config.from_pyfile(config_file, silent=True)
