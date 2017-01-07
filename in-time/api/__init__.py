04495c20286d8f8a66e01da3312ad28987fa176e
Luiz Maestri <luizricardomaestri@gmail.com>
2017-01-07 02:45:03 -0200

    start all api

--- in-time/api/__init__.py
+++ in-time/api/__init__.py
@@ -1,1 +1,12 @@
-__all__ = []
+from os import listdir, path
+from time import sleep
+
+api_module = './api'
+
+__all__ = [
+    filename.replace('.py', '')
+    for filename in listdir(api_module)
+    if not path.isdir(
+        path.join(api_module, filename)
+    ) and filename != '__init__.py'
+]
