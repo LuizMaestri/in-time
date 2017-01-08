3e2cf047d12ed5db10a84c6e2fc4deaa61b0ac25
Luiz Maestri <luizricardomaestri@gmail.com>
2017-01-07 22:28:37 -0200

    create a simple authentication method

--- in-time/api/auth.py
+++ in-time/api/auth.py
@@ -1,2 +1,12 @@
-from model.user import User
 from application.flask_config import app
+from model.user import User
+from flask import request
+
+
+@app.route('/auth', methods=['GET', 'POST'])
+def authenticate():
+    user = User(**request.data)
+    return {
+        'authenticate': User.query.authenticate(user),
+        'user': user.to_json()
+    }
--- in-time/model/user.py
+++ in-time/model/user.py
@@ -1,12 +1,23 @@
+from flask_mongoalchemy import BaseQuery
 from mongoalchemy.document import Index
 from db.password import PasswordField
 from db.doc import Doc, db as field
 
 
-class User(Doc):
-        name = field.StringField(max_length=150)
-        username = field.StringField(max_length=50)
-        password = PasswordField()
-        image = field.StringField(required=False)
+class UserQuery(BaseQuery):
 
-        index_username = Index().ascending('username').unique(drop_dups=True)
+    def authenticate(self, user):
+        return self.filter(
+            self.type.username == user.name,
+            self.type.password == user.password
+        ) is not None
+
+
+class User(Doc):
+    query_class = UserQuery
+    name = field.StringField(max_length=150)
+    username = field.StringField(max_length=50)
+    password = PasswordField()
+    image = field.StringField(required=False)
+
+    index_username = Index().ascending('username').unique(drop_dups=True)
