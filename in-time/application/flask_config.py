dba0ccc537cc1f81e0ed4f09b8fcbf87fc973333
LuizMaestri <luizricardomaestri@gmail.com>
2017-02-06 18:33:00 -0200

    delete files

--- in-time/application/flask_config.py
+++
@@ -1,6 +0,0 @@
-from flask_api import FlaskAPI
-import os
-
-app = FlaskAPI('in-time')
-config_file = os.path.join(app.root_path, 'config.cfg')
-app.config.from_pyfile(config_file, silent=True)
--- in-time/db/load_db.py
+++
@@ -1,6 +0,0 @@
-from flask_mongoalchemy import MongoAlchemy
-from application.flask_config import app
-
-db = MongoAlchemy(app)
-Document = db.Document
-DocumentField = db.DocumentField
