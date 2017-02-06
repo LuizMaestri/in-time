27c1f98cf4913ebd0567ba1b1a5bc2ad67b3c29e
LuizMaestri <luizricardomaestri@gmail.com>
2017-02-06 18:37:50 -0200

    rewrite documents base class

--- in-time/db/doc.py
+++ in-time/db/doc.py
@@ -1,28 +1,24 @@
-from db.load_db import *
-from bson.objectid import ObjectId
+from inspect import ismethod
 
 
-class Doc(Document):
+class Document:
 
-    def to_json(self):
-        _dict = self.__dict__['_values']
-        json = {}
-        for k in _dict:
-            if isinstance(_dict[k].value, ObjectId):
-                json[k] = str(_dict[k].value)
-            elif isinstance(_dict[k].value, Doc):
-                json[k] = _dict[k].value.to_json()
-            elif type(_dict[k].value) is list:
-                json[k] = [
-                    item if not isinstance(_dict[k].value, Doc)
-                    else item.to_json() for item in _dict[k].value
-                ]
-            else:
-                json[k] = _dict[k].value
-        return json
-
-
-class DocField(DocumentField):
+    __database__ = ''
 
     def __init__(self):
-        super().__init__(Doc)
+        self._id = None
+        self.a = 1
+
+    def response_json(self):
+        props = {}
+        for name in dir(self):
+            if not name.startswith('__'):
+                value = getattr(self, name)
+                if not ismethod(value):
+                    props[name] = value
+        return props
+
+    def json(self):
+        props = self.response_json()
+        del props['_id']
+        return props
