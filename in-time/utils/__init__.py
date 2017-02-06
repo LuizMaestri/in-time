c75ee9caca2549031309811724c0e1c934468708
LuizMaestri <luizricardomaestri@gmail.com>
2017-02-06 18:34:41 -0200

    create class to hash elements and func to register APIs

--- in-time/utils/__init__.py
+++ in-time/utils/__init__.py
@@ -1,5 +1,17 @@
-from hashlib import md5
+from os import path, listdir
+from hashlib import sha256
+from importlib import import_module
 
 
-def password_md5(password):
-    return md5(password.encode('utf-8')).hexdigest()
+class Hash(str):
+
+    def __repr__(self):
+        return sha256(self.encode('utf-8')).hexdigest()
+
+
+def api_register(app):
+    api = path.join(app.root_path, 'api')
+    apis = [file for file in listdir(api) if not file.startswith('__')]
+    for blueprint in apis:
+        module = import_module('api.%s' % blueprint[:-3])
+        app.register_blueprint(module.api)
