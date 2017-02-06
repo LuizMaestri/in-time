374fa43d4b453b2f08974ced8fba9fc139697f65
LuizMaestri <luizricardomaestri@gmail.com>
2017-02-06 18:38:12 -0200

    config server

--- in-time/server.py
+++ in-time/server.py
@@ -1,5 +1,13 @@
-from application.flask_config import *
-from api import *
+from application.flask_json import FlaskJSON
+from importlib import import_module
+from utils import api_register
+from os import path, environ
+from db.doc import Document
 
-if __name__ == '__main__':
-    app.run(debug=os.environ['FLASK_DEBUG'])
+APP = FlaskJSON(__name__)
+CONFIG_FILE = path.join(APP.root_path, 'config.cfg')
+APP.config.from_pyfile(CONFIG_FILE, silent=True)
+
+api_register(APP)
+
+Dao = import_module('db.dao').Dao
